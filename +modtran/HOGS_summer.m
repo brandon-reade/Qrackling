71f6829db534115d29d17dba77c4dae571171a26
Brandon Reade <bsr4001@hw.ac.uk>
2026-07-10 20:01:38 +0100

    Added MODTRAN data

--- +modtran/HOGS_summer.m
+++ +modtran/HOGS_summer.m
@@ -14,7 +14,7 @@ saveDir = fullfile(repo_root, "+modtran", "JSON_Cases");
 % ----------------------------
 % Location 1: HOGS (Edinburgh)
 % ----------------------------
-utcDT = datetime(2026,6,21,13,0,0,"TimeZone","UTC");                          % (year, month, day, hr, min, second) - summer solstice 21st of June (~4:30 sunrise - for this location in summer UTC = LOCAL - 1)
+utcDT = datetime(2026,6,21,3,0,0,"TimeZone","UTC");                          % (year, month, day, hr, min, second) - summer solstice 21st of June (~4:30 sunrise - for this location in summer UTC = LOCAL - 1)
 
 % Create the HOGS preset
 hogs = modtran.locations.HOGS(); 
@@ -39,7 +39,7 @@ hogs.source = "sun";
 % hogs.lun_phase_deg = [];                                                 % (only if LocationPreset/geometry supports it)
 
 % Aerosol / clouds (summer scenario)
-hogs.visib_km = 5;                                                          % MODTRAN uses 2% contrast visibility
+hogs.visib_km = 10;                                                          % MODTRAN uses 2% contrast visibility
 hogs.clouds = "none";
 hogs.aerosol_model = "urban";
 hogs.strato_model = "background";
